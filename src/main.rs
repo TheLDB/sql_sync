ac2cc9a6cce89e6cf25d5e6ae2a63b142cad01b5
TheLDB <ldb@erikboles.com>
2022-04-21 18:19:12 -0600

    Added description for argument and added new_config -> but were switching to confy :)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use clap::Parser;
+use std::fs::File;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct ISqlSyncArgs { 
     // Args for clap CLI
-    #[clap(long)]
-    cN: Option<String>,
+
+    /// Create New Config - Config Name
+    #[clap(short = 'c', long = "cn")]
+    cn: Option<String>,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = ISqlSyncArgs::parse();
-    println!("{:?}", args);
+    if let Some(cn) = args.cn {
+        let mut new_config = File::create(format!("{}.toml", cn))?;
+    }
+
+    Ok(())
 }
